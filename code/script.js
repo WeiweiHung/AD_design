894a2a3f19408f24fe7a696b816bfcd115acc849
WeiweiHung <weiweihnug@gmail.com>
2023-12-13 09:50:53 +0800

    觸控

--- code/script.js
+++ code/script.js
@@ -5,51 +5,73 @@ let isFacingRight = false;
 
 document.addEventListener('keydown', (event) => {
     if (event.key === 'a' || event.key === 'ArrowLeft') {
-        console.log('Key pressed:', event.key);
-        isWalking = true;
-        isFacingRight = false;
-        updateCharacter();
+        startWalking(false);
     } else if (event.key === 'd' || event.key === 'ArrowRight') {
-        console.log('Key pressed:', event.key);
-        isWalking = true;
-        isFacingRight = true;
-        updateCharacter();
+        startWalking(true);
     }
 });
 
 document.addEventListener('keyup', (event) => {
     if (event.key === 'a' || event.key === 'ArrowLeft' || event.key === 'd' || event.key === 'ArrowRight') {
-        isWalking = false;
-        updateCharacter();
+        stopWalking();
     }
 });
 
 document.addEventListener('contextmenu', (event) => {
-    event.preventDefault(); // 防止右鍵點擊的默認行為
+    event.preventDefault();
+    stopWalking();
+});
+
+// 添加觸控事件處理邏輯
+let touchStartX = 0;
+
+document.addEventListener('touchstart', (event) => {
+    touchStartX = event.touches[0].clientX;
+});
+
+document.addEventListener('touchend', (event) => {
+    const touchEndX = event.changedTouches[0].clientX;
+    const deltaX = touchEndX - touchStartX;
+
+    if (deltaX > 0) {
+        startWalking(true); // 向右移動
+    } else {
+        startWalking(false); // 向左移動
+    }
+});
+
+document.addEventListener('touchcancel', stopWalking);
+
+function startWalking(right) {
+    console.log('Start walking:', right ? 'right' : 'left');
+    isWalking = true;
+    isFacingRight = right;
+    updateCharacter();
+    moveCharacter(right ? 'right' : 'left');
+}
+
+function stopWalking() {
+    console.log('Stop walking');
     isWalking = false;
     updateCharacter();
-});
+}
 
 function updateCharacter() {
     if (isWalking) {
         character.style.backgroundImage = `url('${isFacingRight ? 'walk-right.png' : 'walk-left.png'}')`;
-        console.log('Key pressed:');
-        moveCharacter(isFacingRight ? 'right' : 'left');
-        
     } else {
         character.style.backgroundImage = `url('${isFacingRight ? 'stand-right.png' : 'stand-left.png'}')`;
-        console.log('Key pressed:');
-        
     }
 }
+
 function moveCharacter(direction) {
-    const currentLeft = parseFloat(character.style.left) || 50; // 初始位置為 left: 50%
-    const step = 1; // 移動步長，根據需要調整
+    const currentLeft = parseFloat(character.style.left) || 50;
+    const step = 1;
 
     if (direction === 'right') {
-        character.style.left = `${Math.min(currentLeft + step, 100)}%`; // 限制在右邊界內
+        character.style.left = `${Math.min(currentLeft + step, 100)}%`;
     } else if (direction === 'left') {
-        const boundary = 0; // 螢幕左邊界
+        const boundary = 0;
         if (currentLeft - step >= boundary) {
             character.style.left = `${currentLeft - step}%`;
         }
