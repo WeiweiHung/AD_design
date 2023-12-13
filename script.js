e1c0bd45112a4891494fbd4dc9042bb36fa8b786
WeiweiHung <weiweihnug@gmail.com>
2023-12-13 09:43:43 +0800

    mmove

--- script.js
+++ script.js
@@ -33,9 +33,25 @@ document.addEventListener('contextmenu', (event) => {
 function updateCharacter() {
     if (isWalking) {
         character.style.backgroundImage = `url('${isFacingRight ? 'walk-right.png' : 'walk-left.png'}')`;
-        character.style.transform = isFacingRight ? 'scaleX(1)' : 'scaleX(-1)';
+        console.log('Key pressed:');
+        moveCharacter(isFacingRight ? 'right' : 'left');
+        
     } else {
         character.style.backgroundImage = `url('${isFacingRight ? 'stand-right.png' : 'stand-left.png'}')`;
-        character.style.transform = isFacingRight ? 'scaleX(1)' : 'scaleX(-1)';
+        console.log('Key pressed:');
+        
+    }
+}
+function moveCharacter(direction) {
+    const currentLeft = parseFloat(character.style.left) || 50; // 初始位置為 left: 50%
+    const step = 1; // 移動步長，根據需要調整
+
+    if (direction === 'right') {
+        character.style.left = `${Math.min(currentLeft + step, 100)}%`; // 限制在右邊界內
+    } else if (direction === 'left') {
+        const boundary = 0; // 螢幕左邊界
+        if (currentLeft - step >= boundary) {
+            character.style.left = `${currentLeft - step}%`;
+        }
     }
 }
